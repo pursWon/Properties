428b63f1bff7fe5ce21aec41f8e2fc2b25aaae72
Wongi <hpurs95@gmail.com>
2022-07-06 19:23:46 +0900

    Initial Commit

---
+++ Properties/PropertiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertiesApp.swift
+//  Properties
+//
+//  Created by MacBook Air on 2022/07/06.
+//
+
+import SwiftUI
+
+@main
+struct PropertiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
